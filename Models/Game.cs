1df2f549ce929748678e05377dbeb4cf2c79b866
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2021-09-17 23:05:38 -0600

    added models and test project

---
+++ Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bowling.Models {
+    public class Game {
+        public int Id { get; set; }
+        public int TotalScore { get; set; }
+        public List<Player> Players { get; set; }
+        
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bowling.Models {
+    public class Player : Person {
+        public int PlayerId { get; set; }
+        public IEnumerable<Frame> Frames { get; set; }
+      
+    }
+}
