67123f98df65475c9afad9bcf26b7b225d7ceea7
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2021-09-24 19:48:40 -0600

    Revert "set up back-end project structure"
    This reverts commit 8ff29c57bdb1437ba24fdd2a99336b7024cca466.

---
+++ Models/Frame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bowling.Models {
+    public class Frame {
+        public int FrameId { get; set; }
+        public List<Roll> Rolls { get; set; } //
+
+    }
+}
