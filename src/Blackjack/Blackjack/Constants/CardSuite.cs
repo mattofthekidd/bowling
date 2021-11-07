543d8f9481cf7ce742341fd1f4d8ad85b82e9195
Aerandor <aerandorfanyi@gmail.com>
2021-11-07 16:22:43 -0700

    New Project

---
+++ src/Blackjack/Blackjack/Constants/CardSuite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Blackjack.Constants {
+	public class CardSuite {
+		public const string DIAMONDS = "Diamonds";
+		public const string HEARTS = "Hearts";
+		public const string SPADES = "Spades";
+		public const string CLUBS = "Clubs";
+
+		public static readonly IList<string> SuiteNames = new ReadOnlyCollection<string>(new List<string> {
+			DIAMONDS,
+			HEARTS,
+			SPADES,
+			CLUBS
+		});
+		}
+}
---
+++ src/Blackjack/Blackjack/Models/DeckModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Models {
+	public class DeckModel {
+		public List<CardModel> Cards { get; set; }
+		public int DeckId { get; set; }
+	}
+}
